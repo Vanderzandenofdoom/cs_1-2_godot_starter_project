85085a17b52abe93349ddec1f5d879cb1deba346
Vanderzandenofdoom <mvanderzanden8029@student.pps.net>
2025-10-01 09:50:04 -0700

    begins
    working on enemy projectile firing. didnt get very far. sadly.

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 
 func _ready():
 	
@@ -7,14 +7,14 @@ func _ready():
 
 func _process(delta: float) -> void:
 	pass
-
-
 	
-
-
+	
+	
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	pass # Replace with function body.
+# TODO: Create a new projectile instance
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
