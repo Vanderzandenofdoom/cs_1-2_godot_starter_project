85e409cc312dfea6c5d609983c480c90959a92a3
Vanderzandenofdoom <mvanderzanden8029@student.pps.net>
2025-10-03 10:02:25 -0700

    stuff
    worked on enemy shoot

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -4,4 +4,6 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
+	if body.name == "Player":
+		body.change_health(-2)
 	pass
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,18 +1,36 @@
 extends CharacterBody2D
 var projectile_original = preload("res://scenes/enemy_projectile.tscn")
-
+var Player
+var timerstart = 2
+var timer = timerstart
+var inrange = false
 func _ready():
 	
 	pass
 
 func _process(delta: float) -> void:
+	if inrange:
+		timer-=delta
+		if timer < 0:
+			shoot(Player.position)
 	pass
 	
 	
 	
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	Player = body
+	if body == Player:
+	var projectile_clone = projectile_original.instantiate()
 	
+	# TODO: Set projectile position to player position
+	projectile_clone.global_position = position
+	
+	# TODO: Set projectile direction using facing variable
+	projectile_clone.set_direction(Player.position)
+	
+	# TODO: Add projectile to the game world
+	get_tree().get_root().add_child(projectile_clone)
 	pass # Replace with function body.
 # TODO: Create a new projectile instance
 
