9e0ddde690b21d7f9f527b6e0b5ff0b68b086e38
Vanderzandenofdoom <mvanderzanden8029@student.pps.net>
2025-10-07 09:30:46 -0700

    shoot code for enemy
    turret shoot code "works"...

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 var projectile_original = preload("res://scenes/enemy_projectile.tscn")
-var Player
+var player
 var timerstart = 2
 var timer = timerstart
 var inrange = false
@@ -12,28 +12,25 @@ func _process(delta: float) -> void:
 	if inrange:
 		timer-=delta
 		if timer < 0:
-			shoot(Player.position)
+			shoot(player)
 	pass
 	
-	
+func shoot(body):
+	if body.name == "Player":
+		var projectile_clone = projectile_original.instantiate()
+		projectile_clone.global_position = position
+		projectile_clone.set_direction(body.position)
+		get_tree().get_root().add_child(projectile_clone)
 	
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	Player = body
-	if body == Player:
-	var projectile_clone = projectile_original.instantiate()
-	
-	# TODO: Set projectile position to player position
-	projectile_clone.global_position = position
-	
-	# TODO: Set projectile direction using facing variable
-	projectile_clone.set_direction(Player.position)
-	
-	# TODO: Add projectile to the game world
-	get_tree().get_root().add_child(projectile_clone)
-	pass # Replace with function body.
-# TODO: Create a new projectile instance
+	player = body
+	if body.name == "Player":
+		inrange = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		inrange == false
+	
+	pass
