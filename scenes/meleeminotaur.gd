fe12b1f36582399cca71d1e97fbcd8578d2dc1b2
Vanderzandenofdoom <mvanderzanden8029@student.pps.net>
2025-11-12 09:50:54 -0800

    updated movement for minotuar
    got chase working. begun on animations.

--- scenes/meleeminotaur.gd
+++ scenes/meleeminotaur.gd
@@ -7,8 +7,12 @@ var health = 3
 var speed = 200
 var startime = 1
 var timer = startime
+var xDirection = 0
+var yDirection = 0
+var facing = "down"
 @onready var player: CharacterBody2D = %Player
 var projectile_original = preload("res://scenes/enemy_arrow.tscn")
+@onready var _animation_player: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if in_range:
@@ -16,14 +20,64 @@ func _process(delta: float) -> void:
 			shoot()
 			timer = startime
 		timer -= delta
+	elif chasing:
+		position += position.direction_to(player.position) * speed*delta
 		
-	
-	pass
+			
+		if xDirection > 0:
+			facing = "right"
+		elif xDirection < 0:
+			facing = "left"
+		elif yDirection < 0:
+			facing = "up"
+		elif yDirection > 0:
+			facing = "down"
+func update_animation():
 
-func _on_melee_body_entered(body: Node2D) -> void:
+	if attacking:
+		_animation_player.play("attack_" + facing)
+	else:
+		if velocity.is_zero_approx():
+			_animation_player.play("idle_crossbow_" + facing)
+		elif !velocity.is_zero_approx():
+			_animation_player.play("walk_" + facing)
+
+
+func _on_shoot_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		in_range = true
+		print("shooting")
+	pass # Replace with function body.
+
+
+func _on_shoot_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = body
 		in_range = false
+	pass # Replace with function body.
+
+
+func _on_chase_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		chasing = true
+		in_range = false
+		print("chasing")
+	pass # Replace with function body.
+
+
+func _on_chase_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		in_range = true
+		chasing = false
+		print("shooting")
+	pass # Replace with function body.
+	
+func _on_melee_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
 		chasing = true
 		attacking = true
 		print("chasing")
@@ -39,44 +93,6 @@ func _on_melee_body_exited(body: Node2D) -> void:
 		print("chasing")
 	pass # Replace with function body.
 
-
-func _on_shoot_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-		in_range = true
-		chasing = false
-		attacking = false
-		print("shooting")
-	pass # Replace with function body.
-
-
-func _on_shoot_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-		in_range = false
-		chasing = false
-		attacking = false
-	pass # Replace with function body.
-
-
-func _on_chase_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-		chasing = true
-		in_range = false
-		attacking = false
-		print("chasing")
-	pass # Replace with function body.
-
-
-func _on_chase_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-		in_range = true
-		chasing = false
-		attacking = false
-		print("shooting")
-	pass # Replace with function body.
 
 func shoot():
 	var projectile_clone = projectile_original.instantiate()
